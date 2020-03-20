7a4ce5368874e46ca69864267ec448060e34c39f
Mark Stabler <mark@markstabler.com>
2020-03-20 01:48:08 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,13 @@
 module Findable
 
+  # can find a particular artist
+  #  def self.find_by_name(name)
+  #    @@artists.detect{|a| a.name == name}
+  #  end
 
+  # can find a particular song
+  #  def self.find_by_name(name)
+  #    @@songs.detect{|a| a.name == name}
+  #  end
 
 end
