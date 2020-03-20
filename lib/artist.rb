b6019b2fa5fff2079423b1c2942c26d29cff3856
Mark Stabler <mark@markstabler.com>
2020-03-20 01:02:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,7 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+# tracks all artists
   @@artists = []
 
   def initialize
@@ -11,27 +12,33 @@ class Artist
     @songs = []
   end
 
+# can find a particular artist
   def self.find_by_name(name)
     @@artists.detect{|a| a.name == name}
   end
 
+# can return all artists
   def self.all
     @@artists
   end
 
+# deletes all artists
   def self.reset_all
     self.all.clear
   end
 
+# can return total number of artists
   def self.count
     self.all.count
   end
 
+# artist instance can add itself as artist to an individual song and track it
   def add_song(song)
     @songs << song
     song.artist = self
   end
 
+# artist instance can add itself as artist to multiple songs and track them
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
