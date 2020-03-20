dd873f3181665f0f3103dc51c43eacb1f782b18c
Mark Stabler <mark@markstabler.com>
2020-03-20 01:32:21 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,4 +1,13 @@
 module Memorable
 
+  # can delete all songs
+  def self.reset_all
+    self.all.clear
+  end
+
+  # can return total number of songs
+  def self.count
+    self.all.count
+  end
 
 end
