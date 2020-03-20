9581ea2458d5c58685f38dc4e0f6207fd6b67264
Mark Stabler <mark@markstabler.com>
2020-03-20 02:04:32 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,13 +1,4 @@
 module Findable
-
-  # tracks all songs
-    all = []
-
-  # can return all songs
-    def all_instances
-      all
-    end
-
   # can find a particular artist
     def find_by_name(name)
       all.detect{|a| a.name == name}
