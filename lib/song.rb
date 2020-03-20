0dbcc1ba260093b2253ef0928ee31587759359ca
Mark Stabler <mark@markstabler.com>
2020-03-20 01:06:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,32 +4,39 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
+# tracks all songs
   @@songs = []
 
   def initialize
     @@songs << self
   end
 
+# can find a particular song
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
   end
 
+# can return all songs
   def self.all
     @@songs
   end
 
+# can delete all songs
   def self.reset_all
     self.all.clear
   end
 
+# can return total number of songs
   def self.count
     self.all.count
   end
 
+# can assign  an artist instance to an individual song
   def artist=(artist)
     @artist = artist
   end
 
+# each song can lowercase a string and change all spaces to hyphens
   def to_param
     name.downcase.gsub(' ', '-')
   end
